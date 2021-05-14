fb7b720364be947fc555e5eee179a81e01b99fc0
Grace liu <grace_liu@Graces-MacBook-Air.local>
2021-05-14 15:57:11 -0700

    Update ocr_core.py

--- ocr_core.py
+++ ocr_core.py
@@ -10,7 +10,7 @@ def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-   
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     file = open("ingredients.txt","w")
     file.write(text)
